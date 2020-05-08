a806cb208abb78ca0c0dcf74cb9ed5b9c8d5aec3
Davide Bellone <davide.bellone>
2020-05-08 17:57:58 +0200

    Add project files.

---
+++ DependencyInjectionScopes/Controllers/GuidMessagesController.cs
@@ -0,0 +1,31 @@
+﻿using DependencyInjectionScopes.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DependencyInjectionScopes.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GuidMessagesController : ControllerBase
+    {
+        private readonly IEnglishGuidMessage englishGuidMessage;
+        private readonly IItalianGuidMessage italianGuidMessage;
+
+        public GuidMessagesController(IItalianGuidMessage italianGuidMessage, IEnglishGuidMessage englishGuidMessage)
+        {
+            this.englishGuidMessage = englishGuidMessage;
+            this.italianGuidMessage = italianGuidMessage;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var messages = new List<string>
+            {
+                italianGuidMessage.GetGuidMessage(),
+                englishGuidMessage.GetGuidMessage()
+            };
+            return messages;
+        }
+    }
+}
---
+++ DependencyInjectionScopes/Services/IGuidGenerator.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace DependencyInjectionScopes.Services
+{
+    public interface IGuidGenerator
+    {
+        Guid GetGuid();
+    }
+
+    public class GuidGenerator : IGuidGenerator
+    {
+        private readonly Guid _guid;
+
+        public GuidGenerator()
+        {
+            _guid = Guid.NewGuid();
+        }
+
+        public Guid GetGuid()
+        {
+            return _guid;
+        }
+    }
+
+    public interface IItalianGuidMessage
+    {
+        string GetGuidMessage();
+    }
+
+    public interface IEnglishGuidMessage
+    {
+        string GetGuidMessage();
+    }
+
+    public class ItalianGuidMessage : IItalianGuidMessage
+    {
+        private readonly IGuidGenerator guidGenerator;
+
+        public ItalianGuidMessage(IGuidGenerator guidGenerator)
+        {
+            this.guidGenerator = guidGenerator;
+        }
+        public string GetGuidMessage() => $"Un messaggio con id {guidGenerator.GetGuid()}";
+    }
+
+
+    public class EnglishGuidMessage : IEnglishGuidMessage
+    {
+        private readonly IGuidGenerator guidGenerator;
+
+        public EnglishGuidMessage(IGuidGenerator guidGenerator)
+        {
+            this.guidGenerator = guidGenerator;
+        }
+        public string GetGuidMessage() => $"A message with id {guidGenerator.GetGuid()}";
+    }
+}
